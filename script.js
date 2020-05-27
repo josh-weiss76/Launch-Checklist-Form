52e554df1d498f94679b52ba080ca38c02333418
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-05-27 10:44:50 -0400

    Added random destination script

--- script.js
+++ script.js
@@ -66,16 +66,17 @@ window.addEventListener("load", function() {
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
       response.json().then(function(json){
          const destination = document.getElementById("missionTarget");
-            destination.innerHTML = `
+         let destinationChoice = Math.round(Math.random()*6);
+         destination.innerHTML = `
             <h2>Mission Destination</h2>
             <ol>
-               <li>Name: ${json[0].name}</li>
-               <li>Diameter: ${json[0].diameter}</li>
-               <li>Star: ${json[0].star}</li>
-               <li>Distance from Earth: ${json[0].distance}</li>
-               <li>Number of Moons: ${json[0].moons}</li>
+               <li>Name: ${json[destinationChoice].name}</li>
+               <li>Diameter: ${json[destinationChoice].diameter}</li>
+               <li>Star: ${json[destinationChoice].star}</li>
+               <li>Distance from Earth: ${json[destinationChoice].distance}</li>
+               <li>Number of Moons: ${json[destinationChoice].moons}</li>
             </ol>
-            <img src="${json[0].image}">
+            <img src="${json[destinationChoice].image}">
             `;
       });
    });
